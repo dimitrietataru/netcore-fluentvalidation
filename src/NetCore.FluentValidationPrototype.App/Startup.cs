509ceb99c8bf994051c7950bd2c3f02cea3f1dd3
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-08-12 12:52:59 +0300

    Add new API project. Clean-up scaffolded code

---
+++ src/NetCore.FluentValidationPrototype.App/Startup.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace NetCore.FluentValidationPrototype.App
+{
+    public sealed class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(configuration);
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(routeBuilder => routeBuilder.MapControllers());
+        }
+    }
+}
