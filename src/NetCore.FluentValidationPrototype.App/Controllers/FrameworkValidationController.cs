509062372cc6c4bc58adc36c0cdeb806b85130f3
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-08-13 17:26:21 +0300

    Add test Controllers and Dtos

---
+++ src/NetCore.FluentValidationPrototype.App/Controllers/FrameworkValidationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore.FluentValidationPrototype.App.Dtos;
+
+namespace NetCore.FluentValidationPrototype.App.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    public sealed class FrameworkValidationController : ControllerBase
+    {
+        [HttpPost]
+        [Route("api/framework-validation/test")]
+        public IActionResult Test([FromBody] WithAttributeDto dto)
+        {
+            return Ok(dto);
+        }
+    }
+}
